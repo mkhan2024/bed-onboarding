9a2b33c289060187722ff9b59f94c7aecadc41e0
Apurba <mkhan14@rrc.ca>
2026-01-10 21:26:37 -0600

    Trigger reviewdog feedback

--- src/broken.ts
+++ src/broken.ts
@@ -1,7 +1,8 @@
-const forceError: number = "not a number";
+const forceError: number = "not a number"; // type mismatch (should trigger lint/ts)
 const unusedVariable = 42; // This variable is declared but not used
 
 export function greet(name) {
+    debugger; // should trigger eslint rule no-debugger
     const message = "Hello, " + name;
     console.log(message);
     return message;
