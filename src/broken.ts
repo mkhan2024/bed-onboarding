9fd5340dd5632430c2da7e804c475ffe89a2b42b
Apurba <mkhan14@rrc.ca>
2026-01-10 22:45:56 -0600

    Add syntax error to trigger Reviewdog feedback

--- src/broken.ts
+++ src/broken.ts
@@ -14,3 +14,4 @@ class Greeter {
         this.message = msg;
     } // Missing property type declaration and parameter type
 }
+const syntaxError = ;  // Intentional syntax error to force ESLint/Reviewdog failure
